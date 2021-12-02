9b512e0502358fa3293b62daa3a90e34d2b39654
pxseu <57842793+pxseu@users.noreply.github.com>
2021-12-02 20:46:01 +0100

    fix(eslint-config): add missing peer dep and make a react config

--- packages/eslint/react.js
+++ packages/eslint/react.js
@@ -2,8 +2,8 @@ module.exports = {
   parserOptions: {
     project: "./tsconfig.eslint.json",
   },
-  plugins: ["import"],
-  extends: ["airbnb-typescript/base", "prettier"],
+  plugins: ["import", "react"],
+  extends: ["airbnb-typescript", "prettier"],
   rules: {
     "@typescript-eslint/quotes": ["error", "double"],
     "@typescript-eslint/indent": ["error", "tab"],
@@ -12,6 +12,7 @@ module.exports = {
     "import/prefer-default-export": "off",
     "consistent-return": "off",
     "no-restricted-syntax": "off",
+    "import/extensions": "off",
   },
   env: {
     es6: true,
